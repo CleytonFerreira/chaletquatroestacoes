a47f92fa8d5329397795eb55304a1cd901ce7d58
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2023-02-19 19:27:30 -0300

    Conclusão do layout mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-//Mostrar/ocultar menu
+//Mostrar/ocultar navbar
 const toggleBtn = document.querySelector('.toggle-btn');
-const menuUl = document.querySelector('.menu > ul');
+const navbarUl = document.querySelector('.navbar > ul');
 
 toggleBtn.addEventListener('click', function () {
   toggleBtn.classList.toggle('open');
-  menuUl.classList.toggle('show');
+  navbarUl.classList.toggle('show');
 });
 
 //Rolagem suave da página
@@ -17,7 +17,37 @@ anchorTags.forEach(anchor => {
     const targetSection = document.querySelector(targetId);
     targetSection.scrollIntoView({ behavior: 'smooth' });
     toggleBtn.classList.remove('open');
-    menuUl.classList.remove('show');
+    navbarUl.classList.remove('show');
   });
 });
 
+//Slider de imagens
+const images = document.querySelectorAll('.slider img');
+let index = 0;
+
+function changeImage() {
+  images[index].classList.remove('active');
+  index = (index + 1) % images.length;
+  images[index].classList.add('active');
+}
+
+setInterval(changeImage, 5000);
+
+//Botão voltar ao topo
+var scrollTopBtn = document.querySelector('.scroll-top-btn');
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add('visible');
+  } else {
+    scrollTopBtn.classList.remove('visible');
+  }
+});
+
+scrollTopBtn.addEventListener('click', function () {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+});
